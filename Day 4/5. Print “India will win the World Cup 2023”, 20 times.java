0b3e83e9128f94366c35ce3c675ddb15a5db90f6
Mohammad Azharuddin <95431771+ahmedazhar262@users.noreply.github.com>
2023-09-29 18:00:36 +0530

    success

---
+++ Day 4/5. Print “India will win the World Cup 2023”, 20 times.java
@@ -0,0 +1,10 @@
+//. Print “India will win the World Cup 2023”, 20 times.
+
+public class Print{
+    public static void main(String [] args){
+
+        for(int i=1; i<=20; i++){
+            System.out.println("India will win the World Cup 2023");
+        }
+    }
+}
