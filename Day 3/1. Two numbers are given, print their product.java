de573a61f65b797060c87c69b1cced705ed99600
Mohammad Azharuddin <95431771+ahmedazhar262@users.noreply.github.com>
2023-09-29 11:06:05 +0530

    success

---
+++ Day 3/1. Two numbers are given, print their product.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Product{
+    public static void main(String [] args){
+        //a and b are already define in input
+        int a= 8;
+        int b= 254;
+
+        //Operation performed
+        int c= a*b;
+
+        System.out.println(c);
+
+    }
+}
