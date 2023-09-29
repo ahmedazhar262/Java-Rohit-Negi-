ea68f97b766914e386e49a8d648dfc7801131295
Mohammad Azharuddin <95431771+ahmedazhar262@users.noreply.github.com>
2023-09-29 09:42:01 +0530

    success

---
+++ Day 2/ Two numbers are given, find their Product.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Product{
+    public static void main(String [] args){
+        int num1= 7;
+        int num2= 67;
+
+        int product= num1*num2;
+
+        System.out.println("The Product of two no are:" + product);
+    }
+}
