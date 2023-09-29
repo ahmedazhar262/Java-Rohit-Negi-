e15f865fe472acae3c2f8381ee3198e3f02e4b80
Mohammad Azharuddin <95431771+ahmedazhar262@users.noreply.github.com>
2023-09-29 10:26:38 +0530

    successsuccess

--- Day 2/ Two numbers are given, find their Product.java
+++ Day 2/ Two numbers are given, find their Product.java
@@ -3,7 +3,6 @@ public class Product{
     public static void main(String [] args){
         int num1= 7;
         int num2= 67;
-
         int product= num1*num2;
 
         System.out.println("The Product of two no are:" + product);
