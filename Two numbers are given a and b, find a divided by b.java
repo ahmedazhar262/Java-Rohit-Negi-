e2e937192b6b87be0079f7236c6f2240404516db
Mohammad Azharuddin <95431771+ahmedazhar262@users.noreply.github.com>
2023-09-29 09:46:30 +0530

    success

---
+++ Two numbers are given a and b, find a divided by b.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Divide{
+    public static void main(String[] args){
+
+        int a= 43;
+        int b= 6;
+
+        int divide= a/b;
+
+        System.out.println("The resultend no after a Divide b are:" + divide);
+    }
+}
