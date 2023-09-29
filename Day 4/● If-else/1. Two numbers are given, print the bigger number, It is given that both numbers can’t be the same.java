493cb000e3061ddbaa4daeb335f082bf5d982b63
Mohammad Azharuddin <95431771+ahmedazhar262@users.noreply.github.com>
2023-09-29 17:05:04 +0530

    changes

---
+++ Day 4/● If-else/1. Two numbers are given, print the bigger number, It is given that both numbers can’t be the same.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Comparison{
+    public static void main(String [] args){
+
+        int a= 7;
+        int b= 9;
+
+
+        if(a==b)
+         System.out.println("Same no can't be define\n Take other Numbers");
+
+        else if (a>b)
+         System.out.println(a+" is greator than "+ b);
+
+        else
+         System.out.println(b+ " is greator than "+ a);
+
+    }
+}
